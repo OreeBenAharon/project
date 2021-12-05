31b1e3df570bccd0e5c91fb520671a91c1fe4e42
OreeBenAharon <orree.ba@gmail.com>
2021-12-05 14:38:10 +0200

    getting ready for netlify

--- server/index.js
+++ server/index.js
@@ -3,19 +3,13 @@ const path = require('path');
 
 const { verify } = require ("./verify");
 
-// const cors = require('cors')
-// const { connectToMongoDB } = require('./config/db')
-// const { initDB } = require('./models/media')
-
 const app = express()
 app.use(express.json())
 app.use(require("cors")())
-// app.use(cors())
+
 require("./db")
 require('dotenv').config()
 
-// connectToMongoDB()
-// initDB()
 
 app.use('/login', require('./routes/login'))
 app.use('/store', require('./routes/store'))
@@ -26,8 +20,28 @@ app.use('/admin', require('./routes/admin'))
 
 app.get('/receptions/:file', verify, (req, res) => {
 
-    res.sendFile(__dirname + `/receptions/${req.params.file}`)
+    // res.download(path.join(__dirname + `/receptions/${req.params.file}`), "123.txt")
+    // res.download(req.params.file)
+    var options = {
+        root: path.join(__dirname, `/receptions/`),
+        headers: {'filename': "123.txt"}
+    }
+    
+    // console.log(req.params.file)
+    // res.sendFile( req.params.file, options)
+    res.download(__dirname + `/receptions/${req.params.file}`, "123.txt")
+    console.log(res)
+    // res.sendFile(path.join(__dirname + `/receptions/${req.params.file}`),{ headers: { 'filename': "123.txt" } },(error) => {console.log(error)})
+    
+
+
 });
+
+// app.get("/receptions/:file", verify, function(req, res) {
+//     res.attachment(__dirname + `/receptions/${req.params.file}`);
+//     res.end('Downloaded', 'UTF-8')
+//  });
+
 // router.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname + '../receptions/reception6.txt'));
 //     // res.sendFile(path.join(__dirname + '../../static/api/api.html'));
